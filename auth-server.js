7d8222a64ae5de41ad5d1fafb08b24fe14ea8bce
Ralica Popova <ralicaterzieva90@gmail.com>
2025-04-10 13:46:34 +0300

    Start authentication server logic

---
+++ auth-server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const session = require('express-session');
+const { Issuer, generators } = require('openid-client');
+const app = express();
+
+let client;
+// Initialize OpenID Client
+async function initializeClient() {
+    const issuer = await Issuer.discover('https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_kesOojGTW');
+    client = new issuer.Client({
+        client_id: 'qb8tnbk5l6lsl09gdi1bhuvp8',
+        client_secret: '<client secret>',
+        redirect_uris: ['https://d84l1y8p4kdic.cloudfront.net'],
+        response_types: ['code']
+    });
+};
+initializeClient().catch(console.error);
+
+app.use(session({
+    secret: 'some secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// routes
+
+const checkAuth = (req, res, next) => {
+    if (!req.session.userInfo) {
+        req.isAuthenticated = false;
+    } else {
+        req.isAuthenticated = true;
+    }
+    next();
+};
