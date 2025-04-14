e35508829cb811cdabc8a885a1460cd89adb8b85
Ralica Popova <ralicaterzieva90@gmail.com>
2025-04-14 11:42:47 +0300

    Modify download_file logic

--- backend/test_s3.py
+++ backend/test_s3.py
@@ -4,4 +4,4 @@ from s3_utils import upload_file, download_file
 upload_file('sample_upload.txt', 'uploads/sample_upload.txt')
 
 # Test download (you can choose a different location locally)
-# download_file('uploads/sample_upload.txt', 'backend/downloaded/sample_download.txt')
+download_file('uploads/sample_upload.txt', 'downloaded/sample_download.txt')
