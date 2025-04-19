21d0268ab7b01cd41d457f2c75d89fe4a2b8666f
Ralica Popova <ralicaterzieva90@gmail.com>
2025-04-19 18:53:33 +0300

    Initialize dynamodb

--- backend/s3_utils.py
+++ backend/s3_utils.py
@@ -11,6 +11,13 @@ s3 = boto3.client(
     region_name=os.getenv("AWS_REGION")
 )
 
+dynamodb = boto3.resource(
+    'dynamodb',
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_REGION")
+)
+
 BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 
 def upload_file(file_path, s3_key):
